2dd4838467561a5aabfc6ce1c75ca3020fbdcf01
Guilherme Gaigher netto <ggaighernt@gmail.com>
2024-10-26 14:35:14 +0000

    refactor: add CORS middleware to allow cross-origin requests

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,10 +4,18 @@ from typing import Dict
 
 from app.api import chat
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(chat.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
